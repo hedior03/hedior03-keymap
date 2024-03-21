263ab94f27ae9ec4da810fde5a260b49cf759b90
Hernan Diaz <hernandiaz035@gmail.com>
2024-03-22 10:20:36 +1100

    Disable unwanted features

--- rules.mk
+++ rules.mk
@@ -1,5 +1,8 @@
-VIA_ENABLE = false
+VIA_ENABLE = no
 ENCODER_MAP_ENABLE = yes
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = no
+NKRO_ENABLE = no
 
 VPATH += keyboards/keychron/common
 SRC += keychron_common.c
